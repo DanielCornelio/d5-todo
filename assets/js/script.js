623dc48e92e4fb6f6cb9afdb4cae7162e7e43201
Daniel Cornelio <jdcornelio89@gmail.com>
2025-02-05 15:06:08 -0600

    se agrega data

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const tasks = [
+    {
+      "id": 1,
+      "descripcion": "Estudiar para el examen de matemáticas",
+      "completada": true
+    },
+    {
+      "id": 2,
+      "descripcion": "Hacer ejercicio por 30 minutos",
+      "completada": false
+    },
+    {
+      "id": 3,
+      "descripcion": "Comprar víveres en el supermercado",
+      "completada": true
+    },
+    {
+      "id": 4,
+      "descripcion": "Leer un capítulo de un libro",
+      "completada": false
+    }
+  ]
