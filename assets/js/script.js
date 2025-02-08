4aa4d8d91b7d4e6af3e16e7d0c2b38dec49e8b77
Daniel Cornelio <jdcornelio89@gmail.com>
2025-02-07 18:20:16 -0600

    se agregan funcionalidades

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,37 @@
 const tasks = [
-    {
-      "id": 1,
-      "descripcion": "Estudiar para el examen de matemáticas",
-      "completada": true
-    },
-    {
-      "id": 2,
-      "descripcion": "Hacer ejercicio por 30 minutos",
-      "completada": false
-    },
-    {
-      "id": 3,
-      "descripcion": "Comprar víveres en el supermercado",
-      "completada": true
-    },
-    {
-      "id": 4,
-      "descripcion": "Leer un capítulo de un libro",
-      "completada": false
-    }
-  ]
+  {
+    "id": 1,
+    "descripcion": "Estudiar para el examen de matemáticas",
+    "completada": true
+  },
+  {
+    "id": 2,
+    "descripcion": "Hacer ejercicio por 30 minutos",
+    "completada": false
+  },
+  {
+    "id": 3,
+    "descripcion": "Comprar víveres en el supermercado",
+    "completada": true
+  },
+  {
+    "id": 4,
+    "descripcion": "Leer un capítulo de un libro",
+    "completada": false
+  }
+]
 
-  const tbody = document.querySelector('.tb')
+const tbody = document.querySelector('.tb')
+const agregarBtn = document.querySelector('.btnAgregar')
+let input = document.querySelector('.inputTask')
 
+
+const mostrarTareas = () => {
   let html = "";
-  tasks.map(task =>{
+  tasks.forEach(task => {
     html += `
       <tr>
-        <td><input type="checkbox" name="" id="" readonly ${task.completada?'checked':''}></td>
+        <td><input type="checkbox" name="" id="" readonly ${task.completada ? 'checked' : ''}></td>
         <td>${task.id}</td>
         <td>${task.descripcion}</td>
         <td></td>
@@ -35,4 +39,33 @@ const tasks = [
       </tr>
     `
   })
-  tbody.innerHTML += html
+  tbody.innerHTML = html
+}
+mostrarTareas()
+
+
+const agregarTarea = (e) => {
+  e.preventDefault()
+
+  const valor = input.value;
+  if (!valor) {
+    alert('Debes ingresar un valor')
+    return
+  }
+
+  const tarea = {
+    id: tasks.length + 1,
+    descripcion: valor,
+    completada: false
+  }
+  tasks.push(tarea)
+  input.value = ''
+  mostrarTareas()
+
+  console.log(tarea)
+  console.log(tasks)
+
+}
+console.log(tasks)
+
+agregarBtn.addEventListener('click', agregarTarea)
